79e1425e08557d9d651f3a448f5d7231063ff6af
anselm <abairdsmith@gmail.com>
2025-03-11 09:36:00 +0100

    Code analysis.

--- android/app/src/main/java/com/anselm/boatcontroller/screens/NavigationScreen.kt
+++ android/app/src/main/java/com/anselm/boatcontroller/screens/NavigationScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
